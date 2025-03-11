4a42e231860653b554c8d75a1f692f58bd1d25a2
nerfthisdev <barm2001@gmail.com>
2025-03-11 15:57:20 +0300

    style: remove unnessesary comment

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -11,7 +11,7 @@ import (
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins (adjust as needed)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
